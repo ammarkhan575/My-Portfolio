23c3e20758569bf19806609d3225f2f6a3b7b84d
ammar <ammarkhan575@gmail.com>
2024-01-15 22:52:09 +0530

    completed contact section and footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Landing from "./scenes/Landing";
 import LineGradient from './components/LineGradient';
 import MySkills from "./scenes/MySkills";
 import Projects from "./scenes/Projects";
+import Contact from "./scenes/Contact";
+import Footer from './scenes/Footer';
+
 function App() {
   const [isTopOfPage, setIsTopOfPage] = useState(true);
   const [selectedPage, setSelectedPage] = useState("home");
@@ -46,9 +49,13 @@ function App() {
       <div className="w-5/6 mx-auto md:h-full">
         <MySkills/>
       </div>
-      <div className="w-5/6 mx-auto md:h-full">
+      <div className="w-5/6 mx-auto">
         <Projects/>
       </div>
+      <div className="w-5/6 mx-auto md:h-full">
+        <Contact/>
+      </div>
+      <Footer />
     </div>
   );
 }
--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -7,7 +7,7 @@ const MySkills = () => {
     const isAboveMediumScreens = useMediaQuery('min-width: 1060px');
 
     return (
-        <section id='#skills' className="pt-10 pb-24">
+        <section id='skills' className="pt-10 pb-24">
             {/* HEADER AND IMAGE SECTION */}
             <div className="md:flex md:gap-16 mt-32 md:justify-between">
                 <motion.div
